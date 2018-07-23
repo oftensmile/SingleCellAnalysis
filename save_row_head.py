dc83cd37d51f19f26ea9bba41796c45a2d2e43a1
Razin Shaikh <razinshaikh@outlook.com>
2018-07-23 14:56:42 +0800

    Add save_row_head.py file

---
+++ save_row_head.py
@@ -0,0 +1,12 @@
+def save_row_head(file):
+    with open(file) as f:
+        data = f.readlines()
+        data = data[1:]
+        row_head = []
+        for line in data:
+            row_head.append(line[:line.index(',')])
+        with open(file.split('.')[0] + '_row.txt', 'w') as f:
+            for i in row_head:
+                f.write(i + '\n')
+    
+    return file.split('.')[0] + '_row.txt'
