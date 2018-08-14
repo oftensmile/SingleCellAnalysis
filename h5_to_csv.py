40b1c26fb15cf1fbeb1887ee371c5ff66706d0f4
Razin Shaikh <razinshaikh@outlook.com>
2018-08-14 22:25:22 +0800

    minor mixes

--- h5_to_csv.py
+++ h5_to_csv.py
@@ -5,18 +5,20 @@ import convert_sparse_to_csv
 import util
 
 def get_list(l):
-    return list(map(lambda x: x.decode('utf-8'), list()))
+    return list(map(lambda x: x.decode('utf-8'), list(l)))
 
 
 def h5_to_csv(input_file):
-    f = h5py.File('GSM2888372_8h.h5')
+    f = h5py.File(input_file)
     output_file = str(input_file.split('.')[:-1])[2:-2]
 
     d = f.get(list(f.keys())[0])
 
     spr = csc_matrix((d['data'][()], d['indices'][()], d['indptr'][()]), shape=tuple(d['shape'][()]))
 
-    convert_sparse_to_csv.split_and_convert(spr, get_list(d['gene_names']), get_list(d['barcodes']), output_file)
+    row_heads = get_list(d['gene_names'])
+    barcodes = get_list(d['barcodes'])
+    convert_sparse_to_csv.split_and_convert(spr, row_heads, barcodes, output_file)
 
 
 def main():
